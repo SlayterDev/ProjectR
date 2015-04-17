bc44ca2e0544de2fe4c7b68edca89e348625c174
Brad Slayter <BradleySlayter@my.unt.edu>
2015-04-17 12:49:34 -0500

    Can now make payments using api

--- app/api.py
+++ app/api.py
@@ -1,7 +1,11 @@
 from flask import redirect, session, url_for, request, g, jsonify
 from flask.ext.httpauth import HTTPBasicAuth
-from app import app
-from .models import User
+from app import app, db
+from .models import User, Transaction, Landlord
+from config import STRIPE_CLIENT_ID, STRIPE_SECRET, STRIPE_PUBLISHABLE, ITEMS_PER_PAGE
+from datetime import datetime
+import requests
+import stripe
 
 auth = HTTPBasicAuth()
 
@@ -24,4 +28,51 @@ def get_resource():
 @auth.login_required
 def get_auth_token():
 	token = g.user.generate_auth_token()
-	return jsonify({'token': token.decode('ascii')})
+	return jsonify({'token': token.decode('ascii'),
+					'landlord': g.user.landlord.property_name,
+					'email': g.user.email})
+
+@app.route('/api/landlord')
+@auth.login_required
+def get_landlord():
+	landlord = g.user.landlord.property_name
+	return jsonify({'landlord': landlord})
+
+@app.route('/api/charge', methods=['POST'])
+@auth.login_required
+def apiCharge():
+	token = request.json['token']
+	amount = request.json['amount']
+
+	landlord = g.user.landlord
+	if landlord is None:
+		return jsonify({'status': 'failure',
+						'reason': 'No landlord set'})
+
+	stripe.api_key = STRIPE_SECRET
+	charge = stripe.Charge.create(
+			amount=amount,
+			currency='usd',
+			source=token,
+			stripe_account=landlord.stripe_id,
+			description=g.user.email,
+			application_fee=int(amount*0.01), # 1% fee
+			statement_descriptor=landlord.property_name)
+
+	print charge
+	if charge['failure_code'] is not None:
+		error = str(charge['failure_message']) + ' ' + str(charge['failure_code'])
+		return jsonify({'status': 'failure',
+						'reason': error})
+
+	# Record charge
+	transaction = Transaction(stripe_charge=charge['id'])
+	transaction.user_id = g.user.id
+	transaction.landlord_id = landlord.id
+	transaction.amount = amount
+	transaction.date = datetime.utcnow()
+
+	db.session.add(transaction)
+	db.session.commit()
+
+	return jsonify({'status': 'success'})
